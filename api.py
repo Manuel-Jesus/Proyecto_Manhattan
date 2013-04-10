9c7ed01a68b82df7c51aca8db77d2aafe0505f18
Manuel-Jesus <manueljesusbeginesgonzalez@gmail.com>
2013-04-10 10:27:33 +0300

    Update api.py
    Se ha modificado el contenido y puesto correctamente el codigo

--- api.py
+++ api.py
@@ -5,9 +5,9 @@ from getpass import getpass
 dominio = raw_input('Introduce el dominio que quieres: ')
 correo = raw_input('Introduce el Nombre de Usuario(usuario@dominion.com): ')
 usuario = raw_input('Introduce el nombre de usuario: ')
-nombrefiltro = raw_input('Introduce el nombre del filtro a crear: ')
-passwd = getpass('Introduce la clave: ')
+passwd = getpass('Introdice la clave: ')
+nombrelabel = raw_input('Introduce el nombre del filtro a crear: ')
 
 client = gdata.apps.emailsettings.client.EmailSettingsClient(domain= dominio)
 client.ClientLogin(email = usuario , password = passwd , source = 'Proyecto Manhattan')
-client.CreateLabel (username = usuario, name = nombrefiltro)
+client.CreateLabel (username = usuario , name = nombrelabel)
