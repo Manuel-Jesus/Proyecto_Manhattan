c40875b2b413c4d00fd6fb85501adac2ddbc69e1
Manuel-Jesus <manueljesusbeginesgonzalez@gmail.com>
2013-04-10 09:28:38 +0300

    Update api.py
    Se ha añadido la linea para crear el filtro, pero aun así da un error
    que hay que averiguar y solucionar

--- api.py
+++ api.py
@@ -3,8 +3,11 @@ from getpass import getpass
 
 
 dominio = raw_input('Introduce el dominio que quieres: ')
-usuario = raw_input('Introduce el Nombre de Usuario(usuario@dominion.com): ')
+correo = raw_input('Introduce el Nombre de Usuario(usuario@dominion.com): ')
+usuario = raw_input('Introduce el nombre de usuario: ')
+nombrefiltro = raw_input('Introduce el nombre del filtro a crear: ')
 passwd = getpass('Introduce la clave: ')
 
 client = gdata.apps.emailsettings.client.EmailSettingsClient(domain= dominio)
 client.ClientLogin(email = usuario , password = passwd , source = 'Proyecto Manhattan')
+client.CreateLabel (username = usuario, name = nombrefiltro)
