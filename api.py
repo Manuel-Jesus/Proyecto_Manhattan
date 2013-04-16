f9d155270922cf523e3273fe85e7b25b8216bb08
Manuel-Jesus <manueljesusbeginesgonzalez@gmail.com>
2013-04-16 22:02:52 +0300

    Update api.py
    Obtener el token

--- api.py
+++ api.py
@@ -1,7 +1,22 @@
 import requests
+from getpass import getpass
 
-peticion = requests.get("https://apps-apis.google.com/a/feeds/emailsettings/2.0/gmail.com/testmanhattan/label")
 
-texto = peticion.text
+usuario=raw_input('Introduce tu usuario(nombre@titaniumsystem.mybiz.com')
+passwd=getpass('Introdice tu clave: ')
 
-print (texto)
+#creamos la valiable con los datos que vamos a pasar posteriormente
+datos={'Email':usuario, 'Passwd':passwd}
+#enviamos los datos para solicitar el token
+ r=requests.post("https://www.google.com/accounts/Clientlogin", datos)
+ 
+ print r.text
+
+
+
+
+#peticion = requests.get("https://apps-apis.google.com/a/feeds/emailsettings/2.0/gmail.com/testmanhattan/label")
+
+#texto = peticion.text
+
+#print (texto)
