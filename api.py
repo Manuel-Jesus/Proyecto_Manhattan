392e582d54dba478abad7ffef977843f57def886
Manuel-Jesus <manueljesusbeginesgonzalez@gmail.com>
2013-05-06 22:40:00 +0300

    Update api.py
    aparente conexion a la api

--- api.py
+++ api.py
@@ -1,25 +1,19 @@
 import requests
 from getpass import getpass
 
+usuario="manueljesus@titaniumsystem.mygbiz.com"
+passwd=raw_input('PASSWORD: ')
 
-usuario=raw_input('Introduce tu usuario(nombre@gmail.com): ')
-passwd=getpass('Introdice tu clave: ')
-
-#creamos la valiable con los datos que vamos a pasar posteriormente
-datos={'Email':usuario, 'Passwd':passwd}
+datos={'Email':usuario, 'Passwd':passwd,'service':'apps'}#service: apps es lo que nos faltaba antes! NOTA: DIFERENCIA MAYUSCULAS DE MINUSCULAS
 #enviamos los datos para solicitar el token
-r=requests.post("https://www.google.com/accounts/Clientlogin", data=datos)
+r=requests.post('https://www.google.com/accounts/Clientlogin', data=datos)
 # En r esta el token
+print r.content #lo que hay que hacer es coger el auth pegarlo en la peticion de abajo en la parte de headers
 
 
-#el .text serviria para mostrarlo como texto
-#print r.text
+#la autentificacion va en las cabeceras
+archivos={'':(open('file.xml','rb'))}
+headers = {'content-type': 'application/atom+xml','Authorization':'GoogleLogin auth='+'AUTH_TOKEN'}
+pet=requests.post('https://apps-apis.google.com/a/feeds/emailsettings/2.0/titaniumsystem.mygbiz.com/manueljesus/label',headers=headers,data=archivos)
 
-
-
-
-datos={'Content-type':'application/atom+xml', 'Authoritation':r.text}
-
-t=requests.get("https://apps-apis.google.com/a/feeds/emailsettings/2.0/titaniumsystem.mygbiz.com/manueljesus/label",data=datos);
-
-print t.text
+print pet.text#comentar si solo se quiere el token
